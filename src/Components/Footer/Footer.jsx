4c211260517830b4609d17c8f2e8ecad20f201bb
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-06-10 16:01:46 -0600

    Adding icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,20 +8,32 @@ const Footer = () => {
             <div className="Container">
                 <div className="Row">
                     <div className="Column">
-                        <a className="FooterLink" href="">About us </a>
+                        <a className="FooterLink" href="">Legal notices </a>
+                        <a className="FooterLink" href="">Terms and conditions</a>
+                    </div>
+                    <div className="Column">
+                        <a className="FooterLink" href="">About us</a>
+                        <a className="FooterLink" href="">Security </a>
                     </div>
                     <div className="Column">
                         <a className="FooterLink" href="">Contact </a>
-                    </div>
-                    <div className="Column">
                         <a className="FooterLink" href="">FAQ </a>
                     </div>
-                    <div className="Row2">
-                        <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/1384/1384063.png" alt="Instagram" /></a>
-                        <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/733/733547.png" alt="Facebook" /></a>
-                        <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/220/220233.png" alt="Twitter" /></a> 
-                        <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/732/732208.png" alt="Google play" /></a>
+                    <div className="Column">
+                        <h5 className="SocialMedia__title">Social media</h5>
+                        <div className="Row2">
+                            <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/1384/1384063.png" alt="Instagram" /></a>
+                            <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/733/733547.png" alt="Facebook" /></a>
+                            <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/220/220233.png" alt="Twitter" /></a> 
+                            <a className="FooterLink col-4" href="FooterLink"><img className="img" src="https://image.flaticon.com/icons/png/512/732/732208.png" alt="Google play" /></a>
+                        </div>
                     </div>
+
+                    <div className="Column">
+                        <img className="logo" src="https://image.flaticon.com/icons/png/512/2898/2898674.png" alt="Carpool icon" />
+                    </div>
+
+
                 </div>
             </div>
         </div>
