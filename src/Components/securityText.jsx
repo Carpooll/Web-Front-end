467d0d99df3255fbbffb0ee624816a92cfb495b3
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-11 15:58:32 -0600

    securitytext component added, no text or styles ready

---
+++ src/Components/securityText.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./style/securityText.css"
+
+class SecurityText extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="container">
+                    <h2>OPTION</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores iusto alias iure quam, facilis quas id, eius harum ipsum perferendis ad consequatur provident quidem corrupti minima fugiat exercitationem, sequi ratione.</p>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default SecurityText
