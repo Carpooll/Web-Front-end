9b467ad269400fcf105ec7579e7006822c686390
Brayan Prieto <a6519150032@utch.edu.mx>
2021-06-10 21:59:09 -0600

    updating the routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react"
 import { BrowserRouter, Route, Switch} from "react-router-dom"
-import Layout from "./pages/Layout/Layout"
+
 
 function App() {
   return ( 
   <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={}></Route>
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route exact path="/" component={}></Route>
+    </Switch>
   </BrowserRouter>  
   );
 }
