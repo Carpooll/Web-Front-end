13e4d00a3c46e4b326315c2efd8e3be55705d639
fertarvega <fertarvega99@gmail.com>
2021-06-10 22:38:25 -0600

    Text position fixed and button added.

--- src/Pages/WhatWeOffer/Whatweoffer.jsx
+++ src/Pages/WhatWeOffer/Whatweoffer.jsx
@@ -12,7 +12,7 @@ class OfferSection extends React.Component{
                             <img src="https://mexico.didiglobal.com/wp-content/uploads/sites/5/2020/06/Driver.png" alt="" className="rounded-circle"/>
                         </div>
                         <div className="col col-lg-4 Offer__section_text">
-                            <h1>Titulo random numero 1000</h1>
+                            <h1>Passenger</h1>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur incidunt facere odit voluptatibus 
                                 earum non provident veritatis accusantium asperiores iusto! Tenetur voluptate nisi doloribus 
                                 commodi sit, minima sequi praesentium cum?</p>
@@ -22,7 +22,7 @@ class OfferSection extends React.Component{
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur incidunt facere odit voluptatibus 
                                 earum non provident veritatis accusantium asperiores iusto! Tenetur voluptate nisi doloribus 
                                 commodi sit, minima sequi praesentium cum?</p>
-                            <button className="btn btn-warning">BECOME A PASSENGER</button>
+                            <button className="btn_passenger">BECOME A PASSENGER</button>
                         </div>
                     </div>
                 </div>
