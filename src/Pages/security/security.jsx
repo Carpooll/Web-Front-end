e09b27a87fb115dd2104bd026b2e2b4978b673e1
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-11 15:46:25 -0600

    security page, styles added, not tested

---
+++ src/Pages/security/security.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import "./Security.css"
+// import SecurityText from "../../Components/securityText"
+
+class Security extends React.Component {
+    render(){
+        return(
+            <React.Fragment>
+
+                <div>
+                    <h1>SECURITY</h1>
+                    {/* <img src="https://images.unsplash.com/photo-1518614768202-663a3a0ecf59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="cab" /> */}
+                </div>
+
+                <div className="text-gp1">
+                    
+                </div>
+                <div className="text-gp1">
+                   
+                </div> 
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default Security;
