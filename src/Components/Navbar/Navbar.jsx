7781af5652239629efc5be5168cbd861825a0bcf
Brayan Prieto <a6519150032@utch.edu.mx>
2021-06-14 17:08:59 -0600

    updating

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ class Navbar extends React.Component{
                                 <p>About</p>
                             </Link>
                         </div>
-                        <div className="Navbar__element">
+                        <div className="Navbar__element last">
                             <Link to="/">
                                 <p>Contact</p>
                             </Link>
