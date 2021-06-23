7874a5290ae8aa4d936ce10928fab8a8256b8e8b
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-23 10:50:00 -0600

    security import name updated on app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-
-import Security from "./Pages/security/Security"
+import Security from "./Pages/Security/Security"
 
 function App() {
   return (
