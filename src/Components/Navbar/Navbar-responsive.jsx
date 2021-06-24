3a8279ecca7a04750b6749e0d7e8cc4d15add0ed
fertarvega <fertarvega99@gmail.com>
2021-06-24 14:25:16 -0600

    Navbar responsive with transparent background and toggle button

--- src/Components/Navbar/Navbar-responsive.jsx
+++ src/Components/Navbar/Navbar-responsive.jsx
@@ -6,28 +6,39 @@ class Navbar extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <nav id="navbar" class="Navbar navbar-expand-lg fixed-top">
-                    <div className="Navbar__img navbar-brand">
+               
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
                         <Link to="/">
-                            <img src="https://www.darwinchambers.com/wp-content/uploads/demo-img.jpg" alt="logo" />
+                          <img className="navbar-logo" src="https://www.darwinchambers.com/wp-content/uploads/demo-img.jpg" alt="logo" />
                         </Link>
-                        <div className="Navbar__title">
-                            <pre>Car<br></br>   Pool</pre>  
-                        </div>  
-                    </div>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse Navbar__elements" id="navbarSupportedContent">
-                        <ul class="navbar-nav nav-pills ml-auto">
-                            <li class="Navbar__element">
-                                <a class="nav-link" href="#what_we_offer">What we offer </a>
-                            </li>
-                            <li class="Navbar__element ">
-                                <a class="nav-link" href="#mdo">Link</a>
-                            </li>
-                        </ul>
-                    </div>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav ml-auto">
+                      <li className="Navbar__element">
+                        <a className="nav-link text-white" href="#">Home</a>
+                      </li>
+                      <li class="Navbar__element">
+                          <a class="nav-link text-white" href="#what_we_offer">What we offer </a>
+                      </li>
+                      <li class="Navbar__element ">
+                          <a class="nav-link text-white" href="#about">About</a>
+                      </li>
+                      <li class="Navbar__element ">
+                          <a class="nav-link text-white" href="#security">Security</a>
+                      </li>
+                      <li class="Navbar__element ">
+                          <a class="nav-link text-white" href="#faq">FAQ</a>
+                      </li>
+                      <li class="Navbar__element ">
+                          <a class="nav-link text-white" href="#about">About</a>
+                      </li>
+                      <li class="Navbar__element ">
+                          <a class="nav-link text-white" href="#contact">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
                 </nav>
             </React.Fragment>
         )
