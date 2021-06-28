5ef9ca5293341e40a82882a5db51db0142163479
fertarvega <fertarvega99@gmail.com>
2021-06-28 16:19:54 -0600

    Fixing home 2 trash

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from "./Pages/Layout/Layout";
 import Main from "./Pages/Main/Main"
 import Home from './Pages/Home/Home'
 import Faq from './Pages/Faq/Faq'
-import Home_2 from "./Pages/Home_2/Home_2";
 import OfferSection from "./Pages/Whatweoffer/Whatweoffer";
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact'
@@ -19,7 +18,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main}></Route>
         <Route exact path="/home" component={Home}></Route>
-        <Route exact path="/home2" component={Home_2}></Route>
         <Route exact path="/faq" component={Faq}></Route>
         <Route exact path="/whatweoffer" component={OfferSection}></Route>
         <Route exact path="/about" component={About}></Route>
