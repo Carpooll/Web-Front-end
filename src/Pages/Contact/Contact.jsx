8867cdcf33d5b1b435e4a35f77371ffaafff9a42
unknown <a6519150007@utch.edu.mx>
2021-06-28 17:02:01 -0600

    Fixing contact container

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,60 +1,72 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 class Contact extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="Contact__background">    
-                    <div className="Color_filter"></div>
-                    <div className="Contact__container mt-5">
-            
-                            <div className="row">
-                                <div className="Contact__header mt-5 mb-2">
-                                    <h1>Talk with us!</h1>
-                                </div>
-                            </div>
-            
-                           <div className="row">
-                               <div className="col-sm-4">
-                                   <div className="Contact__img">
-                                       <img src="https://api.bridge.insure/images/phone-6.jpg" alt="" />
-                                   </div>
-                                   <div className="Contact__text mt-2">
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                   </div>
-                               </div>
-            
-                               <div className="col-sm-4">
-                                   <div className="Contact__img">
-                                       <img src="https://img.icons8.com/wired/2x/ffffff/email.png" alt="" />
-                                   </div>
-                                   <div className="Contact__text mt-2">
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                   </div>
-                               </div>
-            
-                               <div className="col-sm-4">
-                                   <div className="Contact__img">
-                                       <img src="https://www.seekpng.com/png/full/14-144347_location-png-white-vector-free-library-location-icon.png" alt="" />
-                                   </div>
-                                   <div className="Contact__text mt-2">
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                       <p>+52 678121212</p>
-                                   </div>
-                               </div>
-                           </div>
-            
+  render() {
+    return (
+      <React.Fragment>
+        <div className="Contact__background">
+          <div className="Color_filter">
+            <div className="mt-5">
+              <div className="container-fluid">
+                <div className="container">
+                  <div className="row">
+                    <div className="Contact__header mt-5 mb-2">
+                      <h1>Talk with us!</h1>
                     </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col-sm-4">
+                      <div className="Contact__img">
+                        <img
+                          src="https://api.bridge.insure/images/phone-6.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="Contact__text mt-2">
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                      </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                      <div className="Contact__img">
+                        <img
+                          src="https://img.icons8.com/wired/2x/ffffff/email.png"
+                          alt=""
+                        />
+                      </div>
+                      <div className="Contact__text mt-2">
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                      </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                      <div className="Contact__img">
+                        <img
+                          src="https://www.seekpng.com/png/full/14-144347_location-png-white-vector-free-library-location-icon.png"
+                          alt=""
+                        />
+                      </div>
+                      <div className="Contact__text mt-2">
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                        <p>+52 678121212</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </React.Fragment>
-        )
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
