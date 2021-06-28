a975db1f470b4f978c034c6e36ace4cdc4dc4299
fertarvega <fertarvega99@gmail.com>
2021-06-28 16:46:39 -0600

    Home, what we offer and about added to main page

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import Navbar from '../../Components/Navbar/Navbar';
+import Home from "../Home/Home"
+import Whatweoffer from "../Whatweoffer/Whatweoffer"
+import About from "../About/About"
+import Contact from "../Contact/Contact"
 
 class Main extends React.Component {
     render(){
         return (
             <React.Fragment>
-
+                <Home></Home>
+                <Whatweoffer></Whatweoffer>
+                <About></About>
+                <Contact></Contact>
             </React.Fragment>
         )
     }
