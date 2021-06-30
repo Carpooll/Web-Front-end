d3286662b74bd7b478ea4274a7dcafd54dcdb34c
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-30 16:41:20 -0600

    fixed

--- src/Pages/Security/Security.jsx
+++ src/Pages/Security/Security.jsx
@@ -9,7 +9,9 @@ class Security extends React.Component {
         <div className="container-fluid">
           <div className="container">
             <div className="top">
-              <h1 className="title">security</h1>
+              <div className="top_h1">
+                <h1 className="title">security</h1>
+              </div>
               <img className="image" src="https://i.imgur.com/Cg7sMu5.png" alt="cab"/>
             </div>
 
